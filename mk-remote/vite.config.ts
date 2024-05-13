6489f674d81435f8adf7947e836a2c1b1b57abd2
zrt <15970334351@163.com>
2024-05-13 09:33:01 +0800

    feat：esm高阶特性

--- mk-remote/vite.config.ts
+++ mk-remote/vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import federation from "@originjs/vite-plugin-federation";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     federation({
-      name: "remote_app",
-      filename: "remoteEntry.js",
+      name: 'remote_app',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./Button": "./src/components/Button.js",
-        "./App": "./src/App.vue",
-        "./utils": "./src/utils.ts",
+        './Button': './src/components/Button.js',
+        './App': './src/App.vue',
+        './utils': './src/utils.ts',
       },
-      shared: ["vue"],
+      shared: ['vue'],
     }),
   ],
   build: {
-    target: "esnext",
+    target: 'esnext',
     minify: false,
   },
-});
+})
